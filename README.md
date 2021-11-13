c1afc4b7099ec0d0e0a65d34d3cb920febfdcd5b
Michael Huang <mbr.michael@gmail.com>
2021-11-13 20:58:20 +0800

    All new way

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # Trash JPG
 Trash all JPGs inside a folder for mac's Quick Action
 
-## Requirements
-- [homebrew](https://formulae.brew.sh/)
-- [macos-trash](https://github.com/sindresorhus/macos-trash)
+![Quick Action](assets/quick-action.png)
 
-## How to
-1. Install `trash` binary via homebrew, use command `brew install macos-trash` to install
-3. Download `trash-jpg.sh` script
-4. Add execute permission `chmod +x trash-jpg.sh`
-5. Configure Automator
-    ![Automator - Quick Action](./readme-assets/automator-quick-action.png)
-6. Trash JPG will show in Quick Action of a folder
-    ![Quick Action - Trash JPG](./readme-assets/quick-action-trash-jpg.png)
-
+# Compatibility
+MacOS Monterey Shortcuts
 
 ## Logs
-located on `~/trash-jpg.log`
+located on `~/.trash-jpg/trash-jpg.log`
+
+## How to Develop
+Call this script instead of copying, and make sure configuration is same with this screenshot
+```
+~/workspace/mchaelhuang/trash-jpg/trash-jpg.sh $1
+```
+
+![Quick Action Script](assets/quick-action-script-dev.png)
+
+## How to test
+Copy the script into Shortcuts, make sure configuration is same with this screenshot
+
+![Quick Action Script Develop](assets/quick-action-script.png)
--- trash-jpg.sh
+++ trash-jpg.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
-export PATH=$PATH:/usr/local/bin
+# vars
+TRASHJPG_DIR=~/.trash-jpg/
+TRASH_BINARY=${TRASHJPG_DIR}bin/trash
 
-echo "" >> ~/trash-jpg.log
-echo "## $(date) ###################################" >> ~/trash-jpg.log
-echo "Dir: "$1 >> ~/trash-jpg.log
+# create dir if not exist
+[ -d $TRASHJPG_DIR ] || mkdir $TRASHJPG_DIR
+
+# write stdout & stderr into file
+exec >> ${TRASHJPG_DIR}trash-jpg.log
+
+# check binary trash is exist
+if [ ! -f "$TRASH_BINARY" ]; then
+    echo "\n## First Run ###############################"
+    echo "Installing trash binary $TRASH_BINARY"
+
+    # download trash binary
+    curl -L https://github.com/sindresorhus/macos-trash/releases/download/v1.1.1/trash.zip > ${TMPDIR}trash.zip
+
+    # unzip to ./bin/
+    unzip ${TMPDIR}trash.zip -d ${TRASHJPG_DIR}bin/
+
+    echo "Installing done."
+fi
+
+echo "\n## $(date) ###################################"
+echo "Directory: "$1
 cd "$1"
-echo "JPGs trashed: " >> ~/trash-jpg.log
-find . -type f -iname "*.jpg" -maxdepth 1 >> ~/trash-jpg.log
-find . -type f -iname "*.jpg" -maxdepth 1 -exec trash {} \;
+
+# list found
+echo "JPG founds:"
+find . -type f -iname "*.jpg" -maxdepth 1
+
+# find & delete jpg files
+find . -type f -iname "*.jpg" -maxdepth 1 -exec $TRASH_BINARY {} \;
